29a455b6f9618b2bc011ef76ed99df69c93624d9
Mikael Larsson <73987306+84miklar@users.noreply.github.com>
2021-11-12 11:23:18 +0100

    1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace EnvironmentTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
